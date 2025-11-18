c8a586bd104056cf6c7656aa076c667cf554d293
Jonh Look <Axel Lopez Cornejo>
2025-11-17 19:14:26 -0600

    v3.0:Proyecto de Buble sort terminado y funcionando(solo files)

--- BubbleSortFiles.java
+++ BubbleSortFiles.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class BubbleSortFiles{
 
@@ -35,13 +37,13 @@ public class BubbleSortFiles{
     public void bubbleSort(String[] array){
         int n = array.length;
         boolean intercambio;
-
+        
+        // Recorrer todo el arreglo
         for (int i = 0; i < n - 1; i++){
             intercambio = false;
             
-            // Últimos i elementos ya están ordenados
             for (int j = 0; j < n - i - 1; j++){
-
+                // Comparar nombres alfabéticamente (ignorando mayúsculas/minúsculas)
                 if (array[j].compareToIgnoreCase(array[j + 1]) > 0){
                     swap(array, j, j + 1);
                     intercambio = true;
@@ -66,4 +68,49 @@ public class BubbleSortFiles{
             }
         }
     }
+
+    public String[] fileToStringArray(String fileName){
+        File file;
+        FileReader reader;
+        BufferedReader bufer;
+        String linea;
+        String[] array = null;
+        int i = 0;
+        int t;
+        
+        try{
+            t = countFileLines(fileName);
+            array = new String[t];
+            file = new File("C:\\Archivos\\" + fileName);
+            reader = new FileReader(file);
+            bufer = new BufferedReader(reader);
+            
+            
+            while ( (linea = bufer.readLine()) != null ){
+                array[i] = linea.trim(); // trim() elimina espacios extras
+                i++;
+            }
+            reader.close();
+        } catch( Exception e){
+            System.out.println("Error al leer el archivo: " + e.toString());
+        }
+        return array;
+    }
+
+    // Escribir arreglo ordenado de nombres a archivo
+    public void writeStringArrayToFile(String fileName, String[] array){
+        FileWriter file;
+        PrintWriter writer;
+        
+        try{
+            file = new FileWriter("c:\\Archivos\\" + fileName);
+            writer = new PrintWriter(file);
+            for ( String nombre : array )
+                writer.println(nombre);
+            file.close();
+        } catch ( Exception e) {
+            System.out.println("Error al crear el archivo: " + e.toString());
+        }
+    }
+
 }
